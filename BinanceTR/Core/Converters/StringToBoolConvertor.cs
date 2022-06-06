1f5e6cacb3ae3a7fb45e37c174ef2be7742c9280
Mehmet Emin KARADAĞ <emin.karadag@ogr.sakarya.edu.tr>
2022-06-06 23:21:03 +0300

    v1.2.2
    - GetAllOrdersAsync() metodunun tüm sipariş geçmişini getirmesi sağlandı.

---
+++ BinanceTR/Core/Converters/StringToBoolConvertor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BinanceTR.Core.Converters
+{
+    public class StringToBoolConvertor : JsonConverter<bool>
+    {
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetInt32() == 1;
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            throw new InvalidOperationException($"Unable to parse {value} to bool");
+        }
+    }
+}
